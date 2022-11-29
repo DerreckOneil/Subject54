3293a248a23bc60e08f433cc6c3189d16d4a79c1
Derreck Oneil <derreckoneil@gmail.com>
2022-11-28 23:01:15 -0500

    WIP for functionality

---
+++ Assets/Scripts/HUDSystem.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class HUDSystem : MonoBehaviourService
+{
+
+    [SerializeField] private Slider stopTimeMeter;
+    [SerializeField] private Text stopTimeMeterText;
+
+    [SerializeField] private Text vendorText;
+    [SerializeField] private GameObject effectImg;
+
+    [SerializeField] private UnityEvent onRuntimeStart;
+    [SerializeField] private UnityEvent onStopTimeMeterTextEnable;
+    [SerializeField] private UnityEvent onVendorTextEnable;
+    [SerializeField] private UnityEvent onEffectsImgEnable;
+
+    public void OnRuntimeStart()
+    {
+        
+    }
+
+    public void OnStopTimeMeterTextEnable()
+    {
+        stopTimeMeter.enabled = true;
+    }
+
+    public void OnVendorTextEnable()
+    {
+        vendorText.enabled = true;
+    }
+
+    public void OnEffectsImgEnable()
+    {
+        effectImg.GetComponent<Image>().enabled = true;
+    }
+
+
+    private void Start()
+    {
+        Debug.Log("Setting all UI elements off!");
+        //stopTimeMeter.enabled = false;
+        stopTimeMeterText.enabled = false;
+        vendorText.enabled = false;
+        effectImg.GetComponent<Image>().enabled = false;
+    }
+    private void OnEnable()
+    {
+        
+    }
+    private void OnDisable()
+    {
+        
+    }
+}
